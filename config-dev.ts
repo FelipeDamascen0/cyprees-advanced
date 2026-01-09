dfe82c94fa0c1db370e03d512e8b053046ee4876
Felipe Damasceno <felipeodamasceno@hotmail.com>
2026-01-09 18:01:01 -0300

    Config

---
+++ config-dev.ts
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+const baseConfig = require('./cypress.config');
+
+const e2e = {
+    baseUrl: 'https://www.saucedemo.com/',
+    env: {
+        username: 'dev_sauce',
+        password: 'dev_secret'
+    }
+}; 
+
+module.exports = defineConfig({
+    ...baseConfig,
+    e2e
+});
